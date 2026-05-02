66c33bf7f479ccbf98e2d80659b063cf21497771
Arshad Jamal <bharatamir4321@gmail.com>
2026-05-03 02:26:38 +0530

    make some final changes

--- backend/index.js
+++ backend/index.js
@@ -6,15 +6,33 @@ require("./db-connect");
 
 const Router = require("./routes");
 
-var corsOptions = {
-  origin: "http://localhost:5173",
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
+// var corsOptions = {
+//   origin: "http://localhost:5173",
+//   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+// };
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://marketify-atwv.onrender.com",
+];
 
 const app = express();
-app.use(cors(corsOptions));
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("CORS not allowed"));
+      }
+    },
+    credentials: true,
+  }),
+);
+// app.use(cors(corsOptions));
 app.use(express.json());
-app.use("/public", express.static("./public"))
+app.use("/public", express.static("./public"));
 app.use("/api", Router);
 
 let port = process.env.PORT || 8000;
